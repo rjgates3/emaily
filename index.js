fb2ea74373bd4dee5a5871a282248957fc9dbc38
rjgates3 <gates.j.richard@gmail.com>
2019-11-26 13:37:07 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
