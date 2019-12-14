37b4bdc2933e95a1d7acddecc394bf1f8a219c99
rjgates3 <gates.j.richard@gmail.com>
2019-12-14 12:25:24 -0700

    basic header

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,10 @@
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/App';
+import App from './components/App/App';
 import reducers from './reducers';
 
 const store = createStore(reducers, {}, applyMiddleware());
