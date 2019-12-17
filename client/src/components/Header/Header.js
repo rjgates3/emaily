189d50dabf5860a38ecb22ad0a755cd217e28138
rjgates3 <gates.j.richard@gmail.com>
2019-12-16 18:51:54 -0700

    login and logout fully working

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,6 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Header extends React.Component {
+
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return(
+          <li>
+            <a href="/auth/google">
+              Login With Google
+            </a>
+          </li>
+        );
+      default:
+        return( 
+          <li>
+            <a href="/api/logout">
+              Logout
+            </a>
+          </li>
+        );
+    }
+  };
+
   render() {
     return(
       <nav>
@@ -9,9 +34,7 @@ class Header extends React.Component {
               Emaily
           </a>
           <ul className="right">
-            <li>
-              <a>Login With Google</a>
-            </li>
+            { this.renderContent() }
           </ul>
         </div>
       </nav>
@@ -19,4 +42,8 @@ class Header extends React.Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
