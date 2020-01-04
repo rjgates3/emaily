cda1ae776f413a70259dce720a39465394a568ba
rjgates3 <gates.j.richard@gmail.com>
2020-01-04 13:32:59 -0700

    mongoose recipient and survey schemas

---
+++ models/Recipient.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const recipientSchema = new Schema({
+  email: String,
+  responded: { type: Boolean, default: false }
+});
+
+module.exports = recipientSchema;
