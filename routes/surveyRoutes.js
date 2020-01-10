34a6718389fcd2831781d90b0c87fc6b286c6c89
rjgates3 <gates.j.richard@gmail.com>
2020-01-10 11:45:28 -0700

    Mailer class set up with send grid

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -2,6 +2,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
@@ -19,5 +21,8 @@ module.exports = app => {
       dateSent: Date.now()
     });
 
+    // Send an email
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+    mailer.send();
   });
 };
---
+++ services/emailTemplates/surveyTemplate.js
@@ -0,0 +1,4 @@
+'use strict';
+module.exports = (survey) => {
+  return '<div>' + survey.body + '</div>';
+};
